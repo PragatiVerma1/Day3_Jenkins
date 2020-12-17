f52e01da22af6c3e00f5c0f35e5fe6979cab85ca
PragatiVerma1 <76032528+PragatiVerma1@users.noreply.github.com>
2020-12-17 13:07:19 +0530

    Update Program.cs
    Commit after 1st build

--- SampleJenkinsProject/Program.cs
+++ SampleJenkinsProject/Program.cs
@@ -11,6 +11,7 @@ namespace SampleJenkinsProject
         static void Main(string[] args)
         {
             Console.WriteLine("Sample Jenkins");
+            Console.WriteLine("Added after first build");
         }
     }
 }
